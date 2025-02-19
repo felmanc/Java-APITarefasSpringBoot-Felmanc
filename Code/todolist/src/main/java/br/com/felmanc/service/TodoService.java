f9f8442b43f6c9d262d52fc0c23837f8edc4c3d2
felmanc <felmanc@gmail.com>
2025-02-19 14:43:11 -0300

    Criação do serviço

---
+++ Code/todolist/src/main/java/br/com/felmanc/service/TodoService.java
@@ -0,0 +1,63 @@
+package br.com.felmanc.service;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import br.com.felmanc.entity.Todo;
+import br.com.felmanc.repository.TodoRepository;
+
+@Service
+public class TodoService {
+    Logger logger = Logger.getLogger(TodoService.class.getName());
+	
+	@Autowired
+    private TodoRepository todoRepository;
+
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+    
+    public Todo getTodoById(@PathVariable Long id) {
+        return todoRepository.findById(id).orElse(null);
+    }
+
+    public List<Todo> createTodo(@RequestBody Todo todo) {
+        if (!todo.isRealizado()) {
+        	todo.setRealizado(false);
+        }
+
+        if (todo.getPrioridade() == null || todo.getPrioridade().equals(0)) {
+            todo.setPrioridade(1); // valor padrão
+        }
+
+        todoRepository.save(todo);
+        
+        return getAllTodos();
+    }
+    
+    public List<Todo> updateTodo(@RequestBody Todo updatedTodo) {
+        Todo todo = todoRepository.findById(updatedTodo.getId()).orElse(null);
+        if (todo != null) {
+            todo.setNome(updatedTodo.getNome());
+            todo.setDescricao(updatedTodo.getDescricao());
+            todo.setRealizado(updatedTodo.isRealizado());
+            todoRepository.save(todo);
+        }
+        else {
+        	throw new RuntimeException("Entity with id " + updatedTodo.getId() + " not found!");
+        }
+        	
+        return getAllTodos();
+    }
+
+    public List<Todo> deleteTodo(@PathVariable Long id) {
+        todoRepository.deleteById(id);
+        
+        return getAllTodos();
+    }
+}
