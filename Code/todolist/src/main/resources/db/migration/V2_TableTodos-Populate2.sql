81c035e97aa0d63b496ebcae7804aacb0f325143
felmanc <felmanc@gmail.com>
2025-02-21 19:16:19 -0300

    Ajuste do flywaydb/ migrations

---
+++ Code/todolist/src/main/resources/db/migration/V2_TableTodos-Populate2.sql
@@ -0,0 +1,11 @@
+INSERT INTO todos (descricao, nome, prioridade, realizado, data_criacao, data_vencimento) VALUES
+('Estudar para a prova de certificação de Java', 'Estudar Java', 2, 0, '2025-02-18', '2025-02-25'),
+('Redigir o plano de negócios para o novo projeto', 'Plano de Negócios', 3, 0, '2025-02-12', '2025-02-20'),
+('Renovar a assinatura do software de gestão de projetos', 'Renovação de Assinatura', 4, 1, '2025-02-05', '2025-02-10'),
+('Implementar funcionalidades de segurança no sistema', 'Segurança Sistema', 5, 0, '2025-02-15', '2025-03-01'),
+('Participar do treinamento sobre metodologias ágeis', 'Treinamento Ágil', 2, 1, '2025-02-01', '2025-02-03'),
+('Preparar a apresentação para o cliente XYZ', 'Apresentação Cliente', 3, 0, '2025-02-10', '2025-02-15'),
+('Configurar o ambiente de desenvolvimento para o novo projeto', 'Configuração Ambiente', 4, 0, '2025-02-12', '2025-02-13'),
+('Realizar pesquisa de mercado para identificar novas oportunidades', 'Pesquisa de Mercado', 5, 1, '2025-01-25', '2025-02-05'),
+('Atualizar a documentação técnica do sistema', 'Documentação Técnica', 3, 0, '2025-02-07', '2025-02-10'),
+('Participar da reunião de alinhamento com o time de desenvolvimento', 'Reunião Desenvolvimento', 1, 1, '2025-02-15', '2025-02-16');
